c7b0ada198bae58b544d7e351ebe69ebea6bd71c
Emmanuel Yeboah <noelzappy@gmail.com>
2022-10-17 12:54:15 +0000

    Minor changes

--- src/components/PortfolioCreative.jsx
+++ src/components/PortfolioCreative.jsx
@@ -16,8 +16,6 @@ const Portfolio = () => {
     while (currentIndex !== 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-
-      // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex],
         array[currentIndex],

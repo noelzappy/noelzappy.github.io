3ab1f6d2e48ba82f477a4e4b1f08a9dd854f1d88
Emmanuel Yeboah <noelzappy@gmail.com>
2024-03-21 16:02:16 +0000

    Updates

---
+++ _next/static/chunks/app/not-found-614ae418094702fb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160,7],{6478:function(e,n,s){Promise.resolve().then(s.t.bind(s,1749,23)),Promise.resolve().then(s.t.bind(s,5250,23))}},function(e){e.O(0,[647,971,69,744],function(){return e(e.s=6478)}),_N_E=e.O()}]);
